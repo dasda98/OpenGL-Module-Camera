52ea247cb724abc22978f2affc74c0414edd4396
dasda98 <wojniakjakub@gmail.com>
2024-06-23 16:10:26 +0200

    Add Doxygen commnets style

--- include/camera.h
+++ include/camera.h
@@ -35,21 +35,55 @@ typedef struct {
 /**
  * @brief Initialize camera information such as position, speed, angles, etc.
  *
- * @param camera Camera Pointer.
+ * Initializes the camera with specified starting position and default values for other parameters.
+ *
+ * @param camera Pointer to a Camera object.
  * @param position Camera start position.
  */
 void initCamera(Camera* camera, vec3 position);
 
 /**
- * @brief Get view matrix.
- * 
- * @param camera Camera pointer.
- * @param view View matrix.
+ * @brief Get the view matrix for the camera.
+ *
+ * Calculates the view matrix based on the camera's position, front, and up vectors.
+ *
+ * @param camera Pointer to a Camera object.
+ * @param view Output view matrix of type mat4.
  */
 void getViewMatrix(Camera* camera, mat4 view);
 
+/**
+ * @brief Get the projection matrix for the camera.
+ *
+ * Calculates the projection matrix based on the camera's zoom level and the specified aspect ratio.
+ *
+ * @param camera Pointer to a Camera object.
+ * @param projection Output projection matrix of type mat4.
+ * @param aspectRatio Ratio of the screen's width to its height.
+ */
 void getProjectionMatrix(Camera* camera, mat4 projection, float aspectRatio);
+
+/**
+ * @brief Process camera movement via keyboard input.
+ *
+ * Updates the camera's position based on the specified direction and the elapsed time.
+ *
+ * @param camera Pointer to a Camera object.
+ * @param direction Movement direction.
+ * @param deltaTime Time interval between the current and previous frame.
+ */
 void processKeyboard(Camera* camera, Direction direction, float deltaTime);
+
+/**
+ * @brief Process camera movement via mouse input.
+ *
+ * Updates the camera's orientation based on mouse movement.
+ *
+ * @param camera Pointer to a Camera object.
+ * @param xoffset Mouse movement in the X axis.
+ * @param yoffset Mouse movement in the Y axis.
+ * @param constrainPitch If true, limits the pitch angle to prevent camera flip.
+ */
 void processMouseMovement(Camera* camera, float xoffset, float yoffset, bool constrainPitch);
 
 #endif //CAMERA_H
